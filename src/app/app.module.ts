1571d46cd603f79beca474a3e60b2ead0b6b9fc1
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2022-01-05 11:40:18 +0100

    dish service has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { DishService } from './services/dish.service';
 
 
 
@@ -34,7 +35,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MatGridListModule
     
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
